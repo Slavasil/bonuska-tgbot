56f2f3af8dd91c48f0adffa9fc8df304919604e4
Slavasil <subvia@proton.me>
2025-04-19 19:53:26 +0300

    remove some comments

--- SimpleTGBot/Program.cs
+++ SimpleTGBot/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace SimpleTGBot;
+﻿using System.Text;
+
+namespace SimpleTGBot;
 
 public static class Program
 {
     // Метод main немного видоизменился для асинхронной работы
     public static async Task Main(string[] args)
     {
+        // Православная кодировка
+        Console.OutputEncoding = Encoding.UTF8;
+
         TelegramBot telegramBot = new TelegramBot();
         await telegramBot.Run();
     }
